fcb3030689a22fe7c6d3a7e3d64d227ad1da6d3b
Venüs Rüya Gürsoy <ruyagursoy2@gmail.com>
2025-06-03 14:10:49 +0300

    deneme

--- ft_split.c
+++ ft_split.c
@@ -10,4 +10,88 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+size_t count_words(char const *s, char c)
+{
+    size_t  count;
+    int     i;
+    int     j;
+
+    j = 1;
+    i = 0;
+    count = 0;
+    if (c == '\0')
+        return (count);
+    while (s[i] == c)
+        i++;
+    while (s[i])
+    {
+        count++;
+        while(s[i] && s[i] != c)
+            i++;
+        while(s[i] == c)
+            i++;
+    }
+    return (count);
+}
+char **free_str(char **str, size_t index)
+{
+    size_t i;
+
+    i = 0;
+    while (i < index)
+    {
+        free(str[i]);
+        i++;
+    }
+    free(str);
+    return (NULL);
+}
+
+
+char **ft_split(char const *s, char c){
+
+    char    **ret;
+    size_t  i;
+    size_t  index;
+
+    i = 0;
+    index = 0;
+    ret = malloc(sizeof(char *) * (count_words(s, c) + 1));
+    if (!ret)
+        return (NULL);
+    while (*s)
+    {
+        while (*s == c)
+            s++;
+        if (!(*s))
+            break;
+        while (s[i] && s[i] != c)
+            i++;
+        ret[index++] = ft_substr(s, 0, i);
+        if (!ret[index - 1])
+            return (free_str(ret, index - 1));
+        s += i;
+        i = 0;
+    }
+    ret[index] = NULL;
+    return(ret);
+}
+
+// int main()
+// {
+//     char **result;
+//     char str[] = "hello..test..yoyooy.yarak.123.fuki";
+//     printf("%lu\n", count_words("          ", ' '));
+//     result = ft_split("          ", ' ');
+
+//     for (int i = 0; result[i] != NULL; i++)
+//     {
+//         printf("%s\n", result[i]);
+//         free(result[i]);
+//     }
+//     free(result);
+    
+//     return 0;
+// }
